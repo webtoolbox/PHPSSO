6f3beb7f57ccf310bf5b6467784154353ff82267
laeeqwt <laeeq@websitetoolbox.com>
2013-09-23 15:48:10 +0530

    change in comments.

--- login_example.php
+++ login_example.php
@@ -1,13 +1,12 @@
 <?php
 require_once dirname(__FILE__).'/forum_sso_functions.php';
 // Important Note:
-// You also need to update forum username on line 6 in forum_sso_functions.php.
-// You also need to update forum API KEY on line 8 in forum_sso_functions.php.
+// You also need to update forum username on line 7 in forum_sso_functions.php.
+// You also need to update forum API KEY on line 9 in forum_sso_functions.php.
 
 // YOUR CODE HERE.
 
 $user = array();
-
 // Fill in the user information in a way that websitetoolbox forum can understand.
 $user['user'] = '';
 
--- registration_example.php
+++ registration_example.php
@@ -1,8 +1,8 @@
 <?php
 require_once dirname(__FILE__).'/forum_sso_functions.php';
 // Important Note:
-// You also need to update forum username on line 6 in forum_sso_functions.php.
-// You also need to update forum API KEY on line 8 in forum_sso_functions.php.
+// You also need to update forum username on line 7 in forum_sso_functions.php.
+// You also need to update forum API KEY on line 9 in forum_sso_functions.php.
 
 // YOUR CODE HERE.
 
