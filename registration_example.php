47fde1e3292606e9643418eca4ddacd0331dc6c7
webtoolbox <sandeep@websitetoolbox.com>
2018-05-07 13:14:31 -0500

    Added info about email address for username

--- registration_example.php
+++ registration_example.php
@@ -7,6 +7,7 @@ require_once dirname(__FILE__).'/forum_sso_functions.php';
 
 // Your code to process the registration for the user on your website goes here.
 
+// Integrating this is optional. The SSO log in call will automatically create the user's account if it doesn't already exist.
 
 // Fill in the user information in a way that websitetoolbox forum can understand.
 $user = array();
@@ -15,13 +16,14 @@ $user = array();
 // For example: You can assign your register POST/GET values to user array like below:
 // $user['member'] = $_POST['user'].
 
-// Assign username that's displayed on the forum
+// Assign username that's displayed on the forum. 
+// This can be an email address if your website doesn't have usernames. In this case, you should also pass in $user['name'] so that the user's name is shown everywhere on the forum. If no name is provided and the username is an email address, the first part of the email address (before the @) is shown everywhere on the forum.
 $user['member'] = 'john';
 // Assign password for new registration 
 $user['pw'] = 'john123';
 // Assign email id for new registration
 $user['email'] = 'john.php@anonymous.com';	
-// You can assign optional registration fields to '$user' array.
+// You can assign optional registration fields to the '$user' array.
 // For example: $user['name'] = 'John wright';
 
 // You can also assign optional usergroupid field if you want to register user into any specific usergroup (Uses Registered Users or Pending Approval groups by default)
